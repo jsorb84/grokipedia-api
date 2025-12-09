bc38ae10d78ad3cde72e9e2e2b6b64a3bb70eade
Urban <joshuasorber21@gmail.com>
2025-12-09 18:56:28 +0000

    ff

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from collections import defaultdict
 import time
 import os
 from dotenv import load_dotenv
-from discord import Webhook, RequestsWebhookAdapter
+from discord import SyncWebhook
 
 # Load environment variables from .env file
 load_dotenv()  # Add this line to load .env
@@ -256,7 +256,7 @@ async def get_page(
         _cache.popitem(last=False)  # Evict oldest (FIFO)
     _cache[cache_key] = (page, now)
     
-    webhook = Webhook.from_url("https://discord.com/api/webhooks/1371612004803936286/uhqqHO-7diNFx4JDGJxuNV8c3STc5J6YUyaPuWMj1Em_UyMqYB1vqSZ8Bu54LS-Sxk1Z", adapter=RequestsWebhookAdapter())
+    webhook = SyncWebhook.from_url("https://discord.com/api/webhooks/1371612004803936286/uhqqHO-7diNFx4JDGJxuNV8c3STc5J6YUyaPuWMj1Em_UyMqYB1vqSZ8Bu54LS-Sxk1Z")
     webhook.send(content=f"Title: {page_dict.title}")
 
     return page
